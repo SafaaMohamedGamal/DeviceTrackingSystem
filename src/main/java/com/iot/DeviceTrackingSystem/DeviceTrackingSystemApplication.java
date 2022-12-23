317452c8905767b660840db4a287b534b60b0bb7
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2022-12-23 03:11:11 +0200

    Initial Commit

---
+++ src/main/java/com/iot/DeviceTrackingSystem/DeviceTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iot.DeviceTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot/DeviceTrackingSystem/DeviceTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.DeviceTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
